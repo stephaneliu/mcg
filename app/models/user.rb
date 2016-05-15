c421e0d099d4bee071a7684249bd94bf8c8a7f4b
Stephane Liu <sliu@sjliu.com>
2016-05-14 17:34:00 -1000

    Configure devise with sidekiq

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,8 @@ class User < ActiveRecord::Base
     :rememberable, :trackable, :validatable, :confirmable
 
   has_and_belongs_to_many :oauth_credentials
+
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
 end
