e42f4f1e4b7bb0526aef141242532ec62e41ba38
Stephane Liu <sliu@sjliu.com>
2016-05-31 20:46:42 -1000

    Add user confirmation to seed

--- db/seeds/development/user.seeds.rb
+++ db/seeds/development/user.seeds.rb
@@ -1,5 +1,6 @@
 User.find_or_create_by!(email: 'sliu@dev.com') do |user|
   user.password = 'testtest'
+  user.password_confirmation = 'testtest'
   user.confirm
   user.admin!
   puts "Created user: #{user.email} / testtest"
