2ed480a78d2162a9a67f82f2aeac75e44553239e
Stephane Liu <sliu@sjliu.com>
2016-08-28 12:33:48 -1000

    Update lets encrypt verfication code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def letsencrypt
     authorize :visitor
-    render text: "drxppHtGR401yLkRh5egdQwY5da69InRO5Y.LE46GeuZQg4UJOqQNUCubU_FL_CIojASYkTCj0gVYww"
+    render text: "ABf-4A2iIolJIqZooAYNl5_vcoyQuCjbfoUh9IuS5pk.LE46GeuZQg4UJOqQNUCubU_FL_CIojASYkTCj0gVYww"
   end
 end
